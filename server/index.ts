fc7d68fb9499a925c3aa9e8f0c7a0fed0a902028
Rafael Jacinto <rpdjacinto@gmail.com>
2023-03-07 13:42:05 -0800

    basic server and api endpoint

---
+++ server/index.ts
@@ -0,0 +1,45 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express, { Express, Request, Response } from "express"
+
+import { getSummonerByName, getMatchesByPuuid, getMatchById } from './services/riot-api/index.js'
+
+dotenv.config()
+
+const server: Express = express()
+
+server.get("/summoners/:summonerId/match-history", async (req: Request, res: Response) => {
+
+  const summonerData = await getSummonerByName(req.params.summonerId)
+  const recentMatchIds = await getMatchesByPuuid(summonerData.puuid)
+
+  const matches = await Promise.all(
+    recentMatchIds.map((matchId: String) => getMatchById(matchId))
+  )
+
+  const matchHistoryResults = matches.map((match) => {
+    const participant = match.info.participants.find(
+      (participant) => participant.puuid == summonerData.puuid
+    )
+    return {
+      // outcome: String
+      gameDuration: match.info.gameDuration,
+      summonerName: participant.summonerName,
+      // summonerSpells: Array<number>
+      // summonerPerks: Array<number>
+      championId: participant.championId,
+      championName: participant.championName,
+      kills: participant.kills,
+      deaths: participant.deaths,
+      assists: participant.assists,
+      championLevel: participant.championLevel
+    }
+  })
+
+  res.json({
+    "results": matchHistoryResults
+  })
+})
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
---
+++ server/services/riot-api/index.ts
@@ -0,0 +1,39 @@
+
+const getBaseUrl = (region: "na1" |  "americas" = "na1") => `https://${region}.api.riotgames.com`
+
+const getSummonerByNameRoute = (name: String) => `lol/summoner/v4/summoners/by-name/${name}`
+const getMatchesByPuuidRoute = (puuid: String) => `lol/match/v5/matches/by-puuid/${puuid}/ids`
+const getMatchByIdRoute = (matchId: String) => `lol/match/v5/matches/${matchId}`
+
+const apiCall = async (
+  url: String,
+  queryParams: any = {},
+  region: "na1" | "americas" = "na1"
+) => {
+  return fetch(
+    `${getBaseUrl(region)}/${url}?` + new URLSearchParams({
+      ...queryParams,
+      api_key: process.env.RIOT_API_KEY
+    }),
+    {
+      method: "GET"
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+          console.error(error);
+    })
+}
+
+export const getSummonerByName = async (name: string) => {
+  return apiCall(getSummonerByNameRoute(name))
+}
+
+export const getMatchesByPuuid = async (puuid: String) => {
+  return apiCall(getMatchesByPuuidRoute(puuid), {
+    count: 5
+  }, "americas")
+}
+
+export const getMatchById = async (matchId: String) => {
+  return apiCall(getMatchByIdRoute(matchId), {}, "americas")
+}
